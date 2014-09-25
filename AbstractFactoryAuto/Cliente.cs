7731430a5dc52c53a631205703da6046fb366d5b
Los Magios <mauriciopb93@gmail.com>
2014-09-25 18:13:30 -0500

    Carga por primera vez

---
+++ AbstractFactoryAuto/Cliente.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AbstractFactoryAuto.Mundo.Interfaces;
+using AbstractFactoryAuto.Mundo.Clases;
+using AbstractFactoryAuto.Mundo.Fabricas;
+
+namespace AbstractFactoryAuto
+{
+    public partial class Cliente : Form
+    {
+
+        private IFabricaDePartes fabrica;
+        private IAuto auto;
+        private ICarroceria carroceria;
+        private IMotor motor;
+
+        public Cliente()
+        {
+            InitializeComponent();
+            cb_tipo.SelectedIndex = 0;
+
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rb_renoult_CheckedChanged(object sender, EventArgs e)
+        {
+    
+        }
+
+        private void cb_tipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_crear_Click(object sender, EventArgs e)
+        {
+            if (rb_mazda.Checked)
+                  fabrica = new FabricaMazda();
+
+            else if (rb_renoult.Checked)
+                fabrica = new FabricaRenoult();
+            else if(rb_chevrolet.Checked) 
+                fabrica = new FabricaChevrolet();
+     
+            
+            auto = fabrica.crearAuto();
+            carroceria = fabrica.crearCarroceria();
+            motor = fabrica.crearMotor();
+
+            if(cb_tipo.SelectedIndex==0)
+                MessageBox.Show(auto.darNombre());
+            if(cb_tipo.SelectedIndex==1)
+                MessageBox.Show(motor.darNombre());
+            if(cb_tipo.SelectedIndex==2)
+                MessageBox.Show(carroceria.darNombre());
+
+
+            label2.Text = "Se creo un auto : " + auto.darNombre();
+        }         
+        private void Cliente_Load(object sender, EventArgs e)
+        {
+        
+        }                      
+    }
+}
---
+++ AbstractFactoryAuto/Mundo/Clases/CarroceriaChevrolet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AbstractFactoryAuto.Mundo.Interfaces;
+
+namespace AbstractFactoryAuto.Mundo.Clases
+{
+    class CarroceriaChevrolet:ICarroceria
+    {
+        public string darNombre()
+        {
+            return "Carroceria Chevrolet";
+        }
+    }
+}
